33ce882be297dcab396dfec289863eb252675a30
Jackson Pires <jackson.pires@gmail.com>
2021-04-12 18:08:44 -0300

    Exemplo 2

--- async-2/pokemon.js
+++ async-2/pokemon.js
@@ -1,11 +1,50 @@
+const appendPokemon = (pokemon) => {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  let img = document.createElement('img');
+
+  img.src = pokemon.sprites.other.dream_world.front_default;
+  li.innerHTML = `${pokemon.name} </br> ${img.outerHTML}`;
+
+  ul.appendChild(li);
+};
+
 const fetchPokemon = () => {
+  fetch('https://pokeapi.co/api/v2/pokemon/squirtle') // Promise
+    .then((response) => { // Resposta completa do servidor
+      response.json().then((data) => {
+        appendPokemon(data);
 
+        fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur') // Promise
+        .then((response) => { // Resposta completa do servidor
+          response.json().then((data) => {
+            appendPokemon(data);
+
+            fetch('https://pokeapi.co/api/v2/pokemon/pikachu') // Promise
+            .then((response) => { // Resposta completa do servidor
+              response.json().then((data) => {
+                appendPokemon(data);
+
+                fetch('https://pokeapi.co/api/v2/pokemon/rattata') // Promise
+                .then((response) => { // Resposta completa do servidor
+                  response.json().then((data) => {
+                    appendPokemon(data);
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
 };
 
-const requestPokemon = (callback) => {
-  setTimeout(() => {
-    callback('Squirtle');
-  }, 3000);
-};
+// JSON - JavaScript Object Notation
+
+// const requestPokemon = (callback) => {
+//   setTimeout(() => {
+//     callback('Squirtle');
+//   }, 3000);
+// };
 
 window.onload = fetchPokemon();
