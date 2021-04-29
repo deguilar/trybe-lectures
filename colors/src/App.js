a3ed1717e22a74bb5ee5ab5874b6191f155e1e97
Henrique Jensen <henriquejensen@hotmail.com>
2021-04-29 14:19:52 -0300

    adicao do exemplo this

---
+++ colors/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TituloDaPagina from './components/TituloDaPagina'
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <TituloDaPagina />
+      </main>
+    );
+  }
+
+}
+
+export default App;
