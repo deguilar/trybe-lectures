2f8e1aed47c63b449a09cda751a18b9690b332c2
Henrique Jensen <henriquejensen@hotmail.com>
2021-04-29 15:07:11 -0300

    ajuste css app.js

--- colors/src/App.js
+++ colors/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import Eventos from './components/Eventos'
-import TituloDaPagina from './components/TituloDaPagina'
 import "./App.css"
 
 import cores from "./data"
-
 class App extends React.Component {
 
   constructor(props) {
@@ -27,13 +24,21 @@ class App extends React.Component {
 
     return (
       <main>
-        <TituloDaPagina />
-        <Eventos />
-
         <section className="container-colors">
-          <input type="text" onChange={this.manipulaTextoFiltro} />
+          <input
+            type="text"
+            placeholder="Digite o nome de uma cor"
+            onChange={this.manipulaTextoFiltro}
+          />
+
           <ul className="colors">
-            {coresFiltradas.map(cor => <li key={cor.value} style={{ color: cor.color }} >{cor.color}</li>)}
+            {coresFiltradas.map(cor => {
+              return (
+                <li key={cor.value} style={{ color: cor.color }} >
+                  {cor.color}
+                </li>
+              )}
+            )}
           </ul>
         </section>
 
