e61b23e697912467a30b981f79e8bc9f9d809336
Henrique Jensen <henriquejensen@hotmail.com>
2021-04-29 14:33:35 -0300

    adiciona evento botao

--- colors/src/components/Eventos.js
+++ colors/src/components/Eventos.js
@@ -8,16 +8,22 @@ class Eventos extends React.Component {
 
     console.log(props)
     this.mostrarTextDigitado = this.mostrarTextDigitado.bind(this)
+    this.mostrarAlerta = this.mostrarAlerta.bind(this)
   }
 
   mostrarTextDigitado(evento) {
     console.log(evento.target.value)
   }
 
+  mostrarAlerta() {
+    alert("Bem vindo")
+  }
+
   render() {
     return (
       <section className="search">
         <input onChange={(evento) => this.mostrarTextDigitado(evento)} />
+        <button onClick={() => this.mostrarAlerta()} >Pesquisar</button>
       </section>
     );
   }
