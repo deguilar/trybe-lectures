394fc9a804040159f777377fa9d9f93f32560d1d
Henrique Jensen <henriquejensen@hotmail.com>
2021-04-30 11:36:24 -0300

    estilo adicionado

--- user-registration/src/App.js
+++ user-registration/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <main className="app">
+
+        <section className="form-user">
+          <h1>Cadastro</h1>
+          <form>
+
+            <div className="input-group">
+              <label>Nome</label>
+              <input type="text" />
+              <span></span>
+            </div>
+
+            <div className="input-group">
+              <label>Email</label>
+              <input type="text" />
+              <span></span>
+            </div>
+
+            <div className="input-group">
+              <label>Password</label>
+              <input type="password" />
+              <span></span>
+            </div>
+
+            <button type="submit">Cadastrar</button>
+          </form>
+        </section>
+
+      </main>
+    );
+  }
 }
 
 export default App;
