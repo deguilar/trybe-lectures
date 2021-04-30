890735fe5599e676b9ed97816bbfdf22e9a95b54
Henrique Jensen <henriquejensen@hotmail.com>
2021-04-30 13:11:01 -0300

    adicao do handle

--- user-registration/src/App.js
+++ user-registration/src/App.js
@@ -3,29 +3,73 @@ import './App.css';
 
 class App extends React.Component {
 
+  constructor() {
+    super()
+
+    this.state = {
+      name: '',
+      password: '',
+      email: '',
+      errorName: '',
+      errorPassword: '',
+      errorEmail: ''
+    }
+  }
+
+  handleValue = (event) => {
+    const { target: { value, name } } = event
+    let newErrorName = ''
+
+    if(name === 'name' && name.length > 10) {
+      newErrorName = "Nome deve ser menor que 10";
+      this.setState({ ...this.state, errorName: newErrorName })
+      return;
+    }
+
+    this.setState({ ...this.state, [name]: value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Nome: ${this.state.name}`)
+  }
+
   render() {
     return (
       <main className="app">
 
         <section className="form-user">
           <h1>Cadastro</h1>
-          <form>
+          <form onSubmit={this.handleSubmit}>
 
             <div className="input-group">
               <label>Nome</label>
-              <input type="text" />
-              <span></span>
+              <input
+                type="text"
+                onChange={this.handleValue}
+                value={this.state.name}
+                name="name"
+              />
+              <span>{this.state.errorName}</span>
             </div>
 
             <div className="input-group">
               <label>Email</label>
-              <input type="text" />
+              <input
+                type="text"
+                onChange={this.handleValue}
+                name="email"
+              />
               <span></span>
             </div>
 
             <div className="input-group">
               <label>Password</label>
-              <input type="password" />
+              <input
+                type="password"
+                onChange={this.handleValue}
+                name="password"
+              />
               <span></span>
             </div>
 
