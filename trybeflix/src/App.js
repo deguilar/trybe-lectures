505032f16dd28ff3d5758a7427ceef80f39be792
Ícaro Harry <icaro.harry@betrybe.com>
2021-05-31 16:14:20 -0300

    Init lecture

---
+++ trybeflix/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import Header from './components/Header';
+
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main className="main">
+          <Player />
+          <Sidebar />
+        </main>
+      </>
+    );
+  }
+}
+
+export default App;
