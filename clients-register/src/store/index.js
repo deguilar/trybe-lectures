2e9292d4ec14dfa009d988318f6a385c6bf44135
Ícaro Harry <icaro.harry@betrybe.com>
2021-06-07 16:15:50 -0300

    Init lecture

---
+++ clients-register/src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import { composeWithDevtools } from 'redux-devtools-extension';
+import rootReducer from '../reducers';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevtools(),
+);
+
+export default store;
