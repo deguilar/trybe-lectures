9011c5ede606c919b7351add6a7dc2a31fcf4d5c
renato <renato@MacBook-Pro-de-renato.local>
2021-09-03 15:31:53 -0300

    Aula 27.2

---
+++ msc-app/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const HTTP_OK_STATUS = 200;
+const PORT = '3000';
+
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
+app.listen(PORT, () => {
+  console.log('🚀 Segura que nossa app tá rodando!');
+});
