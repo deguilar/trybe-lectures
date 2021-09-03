e57247fe1343ff51a4ee3bd43602afc2a0f59efb
vinicius-vasconcelos <viniciussouzav@gmail.com>
2021-09-03 16:20:20 -0300

    Criando arquivo de base de dados

---
+++ 08-09-2021/database.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id: 1,
+    nome: 'Vinicius'
+  },
+  {
+    id: 2,
+    nome: 'Paulo'
+  },
+  {
+    id: 3,
+    nome: 'Kissyla'
+  },
+  {
+    id: 4,
+    nome: 'Italo'
+  },
+  {
+    id: 5,
+    nome: 'Gabriel'
+  },
+  {
+    id: 6,
+    nome: 'Cristiano'
+  },
+  {
+    id: 7,
+    nome: 'Renato'
+  },
+];
