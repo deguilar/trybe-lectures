effb7e58c927f3f21bd5fbde23add531705e7db2
vinicius-vasconcelos <viniciussouzav@gmail.com>
2021-09-09 13:40:16 -0300

    Criando arquivo temporário de base de dados database.js

---
+++ 09-09-2021/database.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Vinicius'
+  },
+  {
+    id: 2,
+    name: 'Neto'
+  },
+  {
+    id: 3,
+    name: 'John'
+  },
+  {
+    id: 4,
+    name: 'Italo'
+  },
+  {
+    id: 5,
+    name: 'Moreira'
+  },
+  {
+    id: 6,
+    name: 'Raissa'
+  },
+  {
+    id: 7,
+    name: 'Cyrillo'
+  },
+];
