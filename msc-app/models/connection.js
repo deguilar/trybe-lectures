e9a6cfaee588d4c778f3e0a234ff58611e8ed36e
Renato Filho <renatosousafilho@gmail.com>
2021-09-26 22:05:45 -0300

    Mudando connection.js para usar singleton

--- msc-app/models/connection.js
+++ msc-app/models/connection.js
@@ -1,16 +1,21 @@
 const { MongoClient } = require('mongodb');
 
-const MONGODB_URL = 'mongodb://127.0.0.1:27017';
+const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
 
-const connection = () => MongoClient
-    .connect(MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+
+let db = null;
+
+const connection = () => {
+    return db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
+    db = conn.db('model_example');
+    return db;
     })
-    .then((conn) => conn.db('musicClass'))
-    .catch((err) => {
-      console.error(err);
-      process.exit();
-    });
+};
 
 module.exports = connection;
