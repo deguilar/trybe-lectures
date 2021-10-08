a63fffc843c15fedb69815d330150d1d0c37c484
renato <renato@MacBook-Pro-de-renato.local>
2021-10-08 16:42:14 -0300

    Aula 29.3 - Sequelize Associations

---
+++ api-products/controllers/productsController.js
@@ -0,0 +1,52 @@
+const { Product } = require('../models');
+
+const getAll = (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.status(200).json(products);
+    })
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    });
+};
+
+const getById = (req, res) => {
+  Product.findByPk(req.params.id)
+    .then((product) => {
+      if (product === null) {
+        return res.status(404).send({ message: 'Usuário não encontrado' });
+      }
+
+      res.status(200).json(product);
+    })
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    });
+};
+
+const create = (req, res) => {
+  const { name, username, email, password, userId } = req.body;
+
+  Product.create({ name, username, email, password, userId })
+    .then((newUser) => {
+      // Separamos a senha do restante do objeto, para que ela não seja retornada na API
+      const { id, name, username, email, createdAt, userId, updatedAt } = newUser;
+
+      res.status(200).json({ id, name, username, email, userId, createdAt, updatedAt });
+    })
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).send({ message: 'Algo deu errado' });
+    });
+};
+
+
+
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
--- api-products/index.js
+++ api-products/index.js
@@ -6,28 +6,11 @@ app.use(bodyParser.json());
 
 const port = 3000
 
-const { Category, Product } = require('./models');
+const productsController = require('./controllers/productsController');
 
-app.get('/categories', async (req, res) => {
-  const categories = await Category.findAll();
-  res.status(200).json(categories);
-});
-
-app.get('/products', async (req, res) => {
-  const products = await Product.findAll();
-  res.status(200).json(products);
-});
-
-// app.get('/categories/:id', async (req, res) => {
-//   const category = await Category.findByPk(req.params.id);
-//   res.status(200).json(category);
-// });
-
-// app.post('/categories', async (req, res) => {
-//   const { name } = req.body;
-//   const category = await Category.create({ name });
-//   res.status(200).json(category);
-// });
+app.get('/products', productsController.getAll);
+app.get('/products/:id', productsController.getById);
+app.post('/products', productsController.create);
 
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
