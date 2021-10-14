de42fa55981be56a1472ac9fd9b7d3eb3968309c
marcelofcdantas <marcelofcd@gmail.com>
2021-10-14 18:28:20 -0300

    migration album

---
+++ SpotiTrybe/migrations/20211014210814-create-table-albums.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('albums', { 
+      id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      releaseDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      artistId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Artists',
+          key: 'id',
+        }
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+     });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('albums');
+
+  }
+};
