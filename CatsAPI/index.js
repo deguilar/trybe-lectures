889619c30d621a0f18be27eed18856303939fab1
renato <renato@MacBook-Pro-de-renato.local>
2021-10-20 16:02:15 -0300

    Versão inicial tudo num lugar só

--- CatsAPI/index.js
+++ CatsAPI/index.js
@@ -1,17 +1,127 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const catController = require('./controllers/catsController');
+const mysql = require('mysql2/promise');
 
-const app = express()
-const port = process.env.PORT || 3000;
+const app = express();
 
-app.use(bodyParser.json());
-
-app.get('/cats', catController.listCats);
-
-app.get('/cats/:id', catController.catDetails);
-
-app.post('/cats', catController.newCat);
+/* O que é isso? */
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+const connection = mysql
+  .createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'cats_api',
+    password: 'root',
+  });
+
+/* Busca todos os gatos */
+app.get('/cats', async (req, res) => {
+  try {
+    const [cats] = await connection.execute('SELECT name, age FROM cats_api.cats');
+
+    const content = cats.reduce((html, cat) => {
+      const {name, age} = cat;
+      return html + `<li>Nome: ${name} - Idade: ${age}</li>`;
+    }, '');
+
+    const htmlBase = `
+      <html>
+        <head>
+          <title>Gatos</title>
+        </head>
+        <body>
+          <ul style="background-color: antiquewhite">
+            ${content}
+          </ul>
+          <form action="/cats" method="POST">
+            <input name="name" type="text">
+            <input name="age" type="number">
+            <button type="submit">Criar novo gato!</button>
+          </form>
+        </body>
+      </html>
+    `;
+    res.send(htmlBase);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('<h2>Erro ao tentar realizar operação</h2>');
+  }
+});
+
+/* Cria um novo gato */
+app.post('/cats', async (req, res) => {
+  const { name, age } = req.body;
+
+  if (typeof name !== 'string' || name.length < 3 || name.length >= 30) {
+    return res.status(400).send(`
+      <h2>O nome digitado não é válido</h2>
+
+      <a href='/cats'>Voltar</a>
+    `);
+  }
+
+  try {
+    await connection.execute(
+      'INSERT INTO cats_api.cats (name, age) VALUES (?,?)',
+      [name, age],
+    );
+
+    res.send(`
+      <h2>Gato criado com sucesso!</h2>
+
+      <a href='/cats'>Voltar</a>
+    `);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('<h2>Erro ao tentar criar o gato</h2>');
+  }
+});
+
+/* Busca um gato por ID */
+
+  app.get('/cats/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const [cat] = await connection.execute(
+        'SELECT name, age FROM cats_api.cats WHERE id = ?',
+        [id]
+      );
+
+      if (!cat) {
+        return res.status(404).send(`
+          <h2>Gato não encontrado :(</h2>
+
+          <a href='/cats'>Voltar</a>
+        `);
+      }
+      
+      const {name, age} = cat[0];
+
+      const content = `<h2>Nome: ${name} - Idade: ${age}</h2>`;
+      const htmlBase = `
+        <html>
+          <head>
+            <title>Detalhes</title>
+          </head>
+          <body>
+            <div style="background-color: antiquewhite">
+              ${content}
+            </div>
+
+            <a href='/cats'>Voltar</a>
+          </body>
+        </html>
+      `;
+  
+      res.send(htmlBase);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('<h2>Erro ao tentar realizar operação</h2>');
+    }
+  });
+
+app.listen(3001, () => {
+  console.log('Ouvindo a porta 3001!');
+});
